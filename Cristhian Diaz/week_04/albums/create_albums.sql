166b13c2e9f30e22c85d02989060322f4d2b5d0f
CDDiaz <cabj_xeneize@hotmail.com>
2021-07-19 10:30:32 +1000

    Sinatra CRUD ActiveRecord

---
+++ Cristhian Diaz/week_04/albums/create_albums.sql
@@ -0,0 +1,9 @@
+CREATE TABLE albums (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  cover TEXT,
+  band_id INTEGER
+);
+
+INSERT INTO albums (name) VALUES ('Constellations');
+INSERT INTO albums (name) VALUES ('Selenium Forest');
